e0eb674b0da87979a05c14fd266f78a4b5b2d6cd
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-07-29 10:04:40 -0300

    Adding project.

---
+++ src/main/java/com/udemy/eurekaserver/currencyEurekaNamingServer/CurrencyEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.eurekaserver.currencyEurekaNamingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyEurekaNamingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/eurekaserver/currencyEurekaNamingServer/CurrencyEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.eurekaserver.currencyEurekaNamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
